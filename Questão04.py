65cdd8bc67f4e87a220b4f047536e2470df59b22
Osivanitalo <osivanitalo18@gmail.com>
2024-11-19 20:11:02 -0300

    input quest 04

--- Questão04.py
+++ Questão04.py
@@ -2,9 +2,9 @@
 
 '''
  O número de dias decorridos entre duas datas é importante em uma infinidade de
-situações da vida cotidiana. Faça um programa que recebe inicialmente dois valores (dia inicial e mês
-inicial), depois mais dois valores (dia final, mês final), ao final deve indicar quantos dias se passaram entre
-as datas (considere que o ano não é bissexto).
+situações da vida cotidiana. Faça um programa que recebe inicialmente dois valores (dia inicial 
+e mês inicial), depois mais dois valores (dia final, mês final), ao final deve indicar quantos 
+dias se passaram entre as datas (considere que o ano não é bissexto).
 
 Exemplos:
 o 02 de 05 até 03 de 05 - 1 dia
@@ -14,3 +14,9 @@ Não esqueça de verificar se a data inicial é menor ou igual à data final.
 
 Dica: conte o número de dias até cada uma das datas e subtraia esses números.
 '''
+
+dia_inicial = float (('Informe o dia inicial (com dois dígitos): '))
+mes_inicial = float (('Informe o mês inicial (com dois dígitos): '))
+dia_final   = float (('Informe o dia final (com dois dígitos): '))
+mes_final   = float (('Informe o mês final (com dois dígitos): '))
+
