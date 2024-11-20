252b6d6c50bc0b242a50f4c42dd66699d2746a21
Osivanitalo <osivanitalo18@gmail.com>
2024-11-20 18:27:13 -0300

    alteração de sinais

--- Questão04.py
+++ Questão04.py
@@ -24,6 +24,6 @@ mes_final   = int(input('Informe o mês final: '))
 if mes_inicial == mes_final:
     diferenca_dias = dia_final - dia_inicial
 else:
-    diferenca_dias = ((mes_final - mes_inicial) * 30) - (dia_final - dia_inicial)
+    diferenca_dias = ((mes_final - mes_inicial) * 30) - (dia_final + dia_inicial)
     #Aqui considerando que todos os meses têm 30 dias.
 print (f'Entre as datas informadas passaram-se {diferenca_dias} dia(s).')
