0dfa87f7d0957630d9cf1b122d63edc72e6d3651
Osivanitalo <osivanitalo18@gmail.com>
2024-11-20 19:27:24 -0300

    input quest05

--- Questão05.py
+++ Questão05.py
@@ -10,7 +10,17 @@ Em um estacionamento, as tarifas são as seguintes (cumulativas):
 Faça um programa que leia o número de minutos que um veículo permaneceu no estacionamento e exiba
 o valor a ser pago pelo responsável.
 
-Como exemplo, considere que o carro ficou 310 minutos no estacionamento; deve pagar: R$ 16,00 (pelas
-duas primeiras horas) + R$ 10,00 (pela terceira e quarta horas) + R$ 6,00 (pela quinta hora e fração da
-sexta hora): total de R$ 32,00
+Como exemplo, considere que o carro ficou 310 minutos no estacionamento; deve pagar: R$ 16,00
+(pelas duas primeiras horas) + R$ 10,00 (pela terceira e quarta horas) + R$ 6,00 
+(pela quinta hora e fração da sexta hora): total de R$ 32,00
 '''
+
+hr_chegada      = float ((input 'Informe a hora da chegada: '))
+min_chegada     = float ((input 'Informe os minutos da chegada: '))
+tot_min_chegada = (hr_chegada * 60) + min_chegada
+
+hr_saida        = float ((input 'Informe a hora da saída: '))
+min_saida       = float ((input 'Informe os minutos da saída: '))
+tot_min_saida   = (hr_saida * 60) + min_saida
+
+total_permanecia = tot_min_chegada + tot_min_saida
